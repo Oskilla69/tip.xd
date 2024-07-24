9dc07b572795a41759ee11d77af3ccd4069c9a96
oskilla69 <oscar.fan1@gmail.com>
2024-07-24 20:37:01 +1000

    added sick new favicon and a loading screen for tips

--- app.vue
+++ app.vue
@@ -2,24 +2,37 @@
   <div class="app">
     <h1 class="tip-title">Leave a tip?</h1>
     <div class="tips-grid">
-      <Tip class="fifteen" tip="15%" />
-      <Tip class="twenty" tip="20%" />
-      <Tip class="twenty-five" tip="25%" />
-      <Tip class="custom" tip="Custom" />
+      <Tip class="fifteen" :tip="`${fifteen}%`" @click="handleTipClick" />
+      <Tip class="twenty" :tip="`${twenty}%`" @click="handleTipClick" />
+      <Tip
+        class="twenty-five"
+        :tip="`${twentyFive}%`"
+        @click="handleTipClick"
+      />
+      <Tip class="fourty" :tip="`${fourty}%`" @click="handleTipClick" />
+      <!-- <Tip class="custom" tip="Custom" /> -->
       <Tip
         ref="noTipRef"
         class="no-tip"
         tip="No Tip"
         @mouseover="handleNoTipClick"
+        @click="handleNoTipClick"
       />
     </div>
+    <LoadingOverlay v-if="loading" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import LoadingOverlay from "./components/LoadingOverlay.vue";
 
 const noTipRef = ref();
+const fifteen = ref(15);
+const twenty = ref(20);
+const twentyFive = ref(25);
+const fourty = ref(40);
+const loading = ref(false);
 
 function handleNoTipClick() {
   noTipRef.value.$el.style.width = "calc(100% - 80px)";
@@ -31,6 +44,26 @@ function handleNoTipClick() {
   noTipRef.value.$el.style.position = "fixed";
   noTipRef.value.$el.style.left = `${randomX}px`;
   noTipRef.value.$el.style.top = `${randomY}px`;
+
+  fifteen.value += 1;
+  twenty.value += 1;
+  twentyFive.value += 1;
+  fourty.value += 1;
+}
+
+function handleTipClick() {
+  loading.value = true;
+  noTipRef.value.$el.style.position = "";
+  noTipRef.value.$el.style.left = "";
+  noTipRef.value.$el.style.top = "";
+  noTipRef.value.$el.style.width = "";
+  setTimeout(() => {
+    loading.value = false;
+    fifteen.value = 15;
+    twenty.value = 20;
+    twentyFive.value = 25;
+    fourty.value = 40;
+  }, 3000);
 }
 </script>
 
@@ -54,7 +87,7 @@ function handleNoTipClick() {
   display: grid;
   grid-template-areas:
     "fifteen twenty twenty-five"
-    "custom custom custom"
+    "fourty fourty fourty"
     "no-tip no-tip no-tip";
   grid-template-rows: 2fr 1fr 1fr;
   gap: 12px;
@@ -71,9 +104,12 @@ function handleNoTipClick() {
 .twenty-five {
   grid-area: twenty-five;
 }
-
+.fourty {
+  grid-area: fourty;
+}
 .no-tip {
   grid-area: no-tip;
+  z-index: 999;
 }
 .custom {
   grid-area: custom;
